2f822b26ceec4889ee7ee22dc1596f1d2db5f9a4
ninoseki <zgok0079@gmail.com>
2018-05-06 09:13:45 +0900

    Add version

---
+++ app/version.rb
@@ -0,0 +1,3 @@
+module SleepWarm
+  VERSION = "0.1.0".freeze
+end
---
+++ spec/sleep_warm_spec.rb
@@ -0,0 +1,5 @@
+describe SleepWarm do
+  it "should have a VERSION constant" do
+    expect(subject.const_get('VERSION')).to_not be_empty
+  end
+end
