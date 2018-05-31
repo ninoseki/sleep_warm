38577c071ec1227e4e44f1d33e3f8617d9757bf3
Manabu Niseki <zgok0079@gmail.com>
2018-05-31 13:07:31 +0900

    Refactored some spec (#5)

--- spec/mrr_spec.rb
+++ spec/mrr_spec.rb
@@ -1,11 +1,8 @@
 describe SleepWarm::MRR do
-
-  let(:mrr) { SleepWarm::MRR.new }
-
   describe "#parse" do
     it "returns an Array" do
-      expect(mrr.rules).to be_instance_of(Array)
-      mrr.rules.each do |rule|
+      expect(subject.rules).to be_instance_of(Array)
+      subject.rules.each do |rule|
         expect(rule.id).to be_a(Integer)
         expect(rule.enable?).to be_truthy.or be_falshy
         expect(rule.note).to be_a(String)
@@ -19,14 +16,14 @@ describe SleepWarm::MRR do
     context "input doesn't match any rule" do
       it "should return false" do
         input = { method: "NOPE", uri: "NOP" }
-        res = mrr.find(input)
+        res = subject.find(input)
         expect(res).to eq(nil)
       end
     end
     context "input matches a rule" do
       it "should return true" do
         input = { method: "GET", uri: "/login" }
-        res = mrr.find(input)
+        res = subject.find(input)
         expect(res).to be_a(SleepWarm::Rule)
         expect(res.response.dig("status")).to be_a(Integer)
         expect(res.response.dig("header")).to be_a(Hash)
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,15 +8,15 @@ RSpec.configure do |config|
   config.shared_context_metadata_behavior = :apply_to_host_groups
 end
 
+def app
+  @app
+end
+
+def mock_app(&builder)
+  @app = Rack::Builder.new(&builder)
+end
+
 RSpec.shared_context "rackapp testing", shared_context: :metadata do
-  def app
-    @app
-  end
-
-  def mock_app(&builder)
-    @app = Rack::Builder.new(&builder)
-  end
-
   before :all do
     @access_log = StringIO.new
     @application_log = StringIO.new
@@ -39,14 +39,6 @@ RSpec.shared_context "rackapp testing", shared_context: :metadata do
 end
 
 RSpec.shared_context "spyup testing", shared_context: :metadata do
-  def app
-    @app
-  end
-
-  def mock_app(&builder)
-    @app = Rack::Builder.new(&builder)
-  end
-
   before :all do
     @access_log = StringIO.new
     @application_log = StringIO.new
@@ -63,14 +55,6 @@ RSpec.shared_context "spyup testing", shared_context: :metadata do
 end
 
 RSpec.shared_context "huntup testing", shared_context: :metadata do
-  def app
-    @app
-  end
-
-  def mock_app(&builder)
-    @app = Rack::Builder.new(&builder)
-  end
-
   before :all do
     @hunting_log = StringIO.new
     hunting_logger = SleepWarm::HuntingLogger.new(@hunting_log)
